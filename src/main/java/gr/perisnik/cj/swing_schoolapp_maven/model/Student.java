362da5b377a09956a9871f4f5afd572efe797225
perisnik <nikouliciousp@gmail.com>
2024-11-18 00:58:50 +0200

    v1.4 Add models

---
+++ src/main/java/gr/perisnik/cj/swing_schoolapp_maven/model/Student.java
@@ -0,0 +1,96 @@
+package gr.perisnik.cj.swing_schoolapp_maven.model;
+
+/**
+ * Represents a student with an ID, firstname, and lastname.
+ * This class provides methods to get and set these properties.
+ * 
+ * @author Peris Nik
+ */
+public class Student {
+    private int id;
+    private String firstname;
+    private String lastname;
+    
+    /**
+     * Default constructor.
+     */
+    public Student() {}
+
+    /**
+     * Constructs a new Student with the specified ID, firstname, and lastname.
+     * 
+     * @param id the ID of the student
+     * @param firstname the firstname of the student
+     * @param lastname the lastname of the student
+     */
+    public Student(int id, String firstname, String lastname) {
+        super();
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    /**
+     * Returns the ID of the student.
+     * 
+     * @return the ID of the student
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets the ID of the student.
+     * 
+     * @param id the new ID of the student
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Returns the firstname of the student.
+     * 
+     * @return the firstname of the student
+     */
+    public String getFirstname() {
+        return firstname;
+    }
+
+    /**
+     * Sets the firstname of the student.
+     * 
+     * @param firstname the new firstname of the student
+     */
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    /**
+     * Returns the lastname of the student.
+     * 
+     * @return the lastname of the student
+     */
+    public String getLastname() {
+        return lastname;
+    }
+
+    /**
+     * Sets the lastname of the student.
+     * 
+     * @param lastname the new lastname of the student
+     */
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    /**
+     * Returns a string representation of the student.
+     * 
+     * @return a string representation of the student
+     */
+    @Override
+    public String toString() {
+        return "Student [id=" + id + ", firstname=" + firstname + ", lastname=" + lastname + "]";
+    }
+}
---
+++ src/main/java/gr/perisnik/cj/swing_schoolapp_maven/model/User.java
@@ -0,0 +1,96 @@
+package gr.perisnik.cj.swing_schoolapp_maven.model;
+
+/**
+ * Represents a user with an ID, username, and password.
+ * This class provides methods to get and set these properties.
+ * 
+ * @author Peris Nik
+ */
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    
+    /**
+     * Default constructor.
+     */
+    public User() {}
+
+    /**
+     * Constructs a new User with the specified ID, username, and password.
+     * 
+     * @param id the ID of the user
+     * @param username the username of the user
+     * @param password the password of the user
+     */
+    public User(int id, String username, String password) {
+        super();
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    /**
+     * Returns the ID of the user.
+     * 
+     * @return the ID of the user
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets the ID of the user.
+     * 
+     * @param id the new ID of the user
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Returns the username of the user.
+     * 
+     * @return the username of the user
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Sets the username of the user.
+     * 
+     * @param username the new username of the user
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * Returns the password of the user.
+     * 
+     * @return the password of the user
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Sets the password of the user.
+     * 
+     * @param password the new password of the user
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * Returns a string representation of the user.
+     * 
+     * @return a string representation of the user
+     */
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", username=" + username + ", password=" + password + "]";
+    }
+}
